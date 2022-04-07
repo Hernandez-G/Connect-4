ed01ab701638af48a03a222a7a3e32019ec0dc59
PX L <g5hernandez@yahoo.com>
2022-04-07 11:24:24 -0700

    game pieces

--- js/main.js
+++ js/main.js
@@ -8,11 +8,9 @@ let result;  //P1/P2, 'T' = TIE, null = G in P
 
 const columnEls = document.querySelectorAll('.column');
 const msgEl = document.querySelector('h3');
-// const markerEls = [...document.querySelectorAll('#markers > div')];
 
 
 document.querySelector('.gameBoard').addEventListener('click', handlePlayerTurn);
-// document.getElementById('markers').addEventListener('click', handleDrop);
 resetBtn.addEventListener('click', init);
 
 
@@ -20,13 +18,13 @@ init();
 
 function init() {
   gameBoard = [
-      [null, null, null, null, null, null], 
-      [null, null, null, null, null, null], 
-      [null, null, null, null, null, null], 
-      [null, null, null, null, null, null], 
-      [null, null, null, null, null, null], 
-      [null, null, null, null, null, null]
-];
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null]
+  ];
   turn = PLAYER1;
   result = null;
 
@@ -34,47 +32,35 @@ function init() {
 }
 
 function handlePlayerTurn(evt) {
-    const rowIdx = parseInt(evt.target.id);
-    const colIdx = parseInt(evt.target.parentElement.id);
-    // console.log(rowIdx, colIdx);
-    if (turn === 1) return;
-    // const colArr = gameBoard[colIdx];
-    console.log(turn);
-    gameBoard[colIdx][rowIdx] = turn;
-    turn = (turn === PLAYER1) ? PLAYER2 : PLAYER1;}
-    if (result) true;   
-    render();
+  if (!evt.target.classList.contains('circle')) return;
+  const colIdx = parseInt(evt.target.parentElement.id);
+  const colArr = gameBoard[colIdx];
+  const rowIdx = colArr.indexOf(null);
+  if (rowIdx === -1) return;
+  console.log(turn);
+  gameBoard[colIdx][rowIdx] = turn;
+  turn = (turn === PLAYER1) ? PLAYER2 : PLAYER1;
+  result = getWinner(colIdx, rowIdx);
+  render();
+}
+
+function getWinner(colIdx, rowIdx) {
+
+}
 
 
+function render() {
+  columnEls.forEach(function (columnEl, colIdx) {
+    Array.from(columnEl.children).forEach(function (rowEl, rowIdx) {
+      rowEl.style.backgroundColor = gameBoard[colIdx][5 - rowIdx];
 
-  function render() {
-    columnEls.forEach(function(columnEl, colIdx) {
-      Array.from(columnEl.children).forEach(function(rowEl, rowIdx) {
-        rowEl.style.backgroundColor = gameBoard[colIdx][rowIdx];
-        // document.getElementsByClassName(`column${colIdx}row${rowIdx}`);
+    })
+  });
 
-      })
-    });
-
-   if (result) {msgEl.innerText = `${result} WINS!`;}
-   else {
-       msgEl.innerText = '';
-   }
-  };
-
-  // function handleDrop(evt) {
-  //   const colIdx = parseInt(evt.target.parentElement.id);
-  //   if (colIdx === -1) return;
-  //   const colArr = gameBoard[colIdx];
-  //   const rowIdx = parseInt(evt.target.id);
-  //   colArr[rowIdx] = turn;
-  //   render();
-  //   turn *= -1;
-  // }
-
-  function renderMarkers() {
-    markerEls.forEach(function(markerEl, colIdx) {
-      markerEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
-      console.log(renderMarkers);
-    });
+  if (result) { msgEl.innerText = `${result} WINS!`; }
+  else {
+    msgEl.innerText = '';
   }
+};
+
+
